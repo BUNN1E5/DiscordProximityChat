1d8742fdd44fd403781dec47daa08c3c6eb33748
Jacob <jacob@goldenneedham.com>
2022-11-05 03:02:16 -0700

    Fixed patch to wrong function

--- DiscordProximityChat/Patches.cs
+++ DiscordProximityChat/Patches.cs
@@ -5,7 +5,7 @@ namespace DiscordProximityChat{
     [HarmonyPatch]
     public class Patches{
         [HarmonyPrefix]
-        [HarmonyPatch(typeof(AudioSignal), nameof(AudioSignal.FrequencyToString))]
+        [HarmonyPatch(typeof(AudioSignal), nameof(AudioSignal.SignalNameToString))]
         public static bool SignalNameToStringPatch(SignalName name, ref string __result){
             if (Constants.ReversePlayerSignals.ContainsKey(name)){
                 __result = Constants.ReversePlayerSignals[name].Name;
