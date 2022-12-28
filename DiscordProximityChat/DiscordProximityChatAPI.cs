564fff09335607e4e620434c485a4b38e5d59407
BUNN1E5 <BUNN1E5@users.noreply.github.com>
2022-12-27 23:20:37 -0800

    Added API Class, no contents yet
    Signed-off-by: BUNN1E5 <BUNN1E5@users.noreply.github.com>

---
+++ DiscordProximityChat/DiscordProximityChatAPI.cs
@@ -0,0 +1,8 @@
+namespace DiscordProximityChat;
+
+public interface DiscordProximityChatAPI{
+    //TODO :: Figure out API lmao
+    
+    //I dunno maybe a way to control who hears who?
+    //What do other mods need access to?
+}
